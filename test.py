1df880b53d8eb6d462fbbc421ba15194073021a4
Grzegorz Preibisch <gp386027@students.mimuw.edu.pl>
2021-01-12 17:28:10 +0100

    after moving lassso inside of gadged.py

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import sumu
 import pandas as pd
 import numpy as np
+
+from glmnet import ElasticNet
+
 import time
 # import glmnet_python
 # from glmnet import glmnet
-
-from glmnet import ElasticNet
 
 start = time.time()
 df = pd.read_csv('test_data.csv')
@@ -14,7 +15,10 @@ arr = np.array(df)
 
 data = sumu.Data(arr, discrete=False)
 
-params = {"data": data,
+params = {
+
+          "array": arr,
+          "data": data,
           "scoref": "bge",  # Or "bdeu" for discrete data.
           # "ess": 10,        # If using BDeu.
           "max_id": -1,
@@ -27,34 +31,8 @@ params = {"data": data,
           "thinning": 10}
 
 g = sumu.Gadget(**params)
-dag, score = g.sample()
+dag = g.sample()
 end = time.time()
-
-
-previous_parent = list(dag[0])
-final_dag = dict()
-for i in range(1, len(dag)):
-    for j in dag[i]:
-        # code fro regression
-        y = arr[:, j]
-        x = np.zeros((arr.shape[0], len(previous_parent)))
-        for col in range(len(previous_parent)):
-            x[:, col] = arr[:, previous_parent[col]]
-        m = ElasticNet()
-        m = m.fit(x, y)
-        #print(m.coef_path_)
-        final_dag[j] = list()
-        for v in range(len(m.coef_)):
-            if m.coef_[v] !=0:
-                final_dag[j].append((previous_parent[v],m.coef_[v])) ## If intersept is 0 and changes order
-    # print(previous_parent, j)
-    previous_parent = list(set(previous_parent).union(dag[i]))
-
-print(final_dag)
-
-# The following only for continuous data
-# dags = [sumu.bnet.family_sequence_to_adj_mat(dag) for dag in dags]
-# causal_effects = sumu.beeps(dags, data)
-# print(causal_effects)
+print(dag)
 
 print(end - start)
